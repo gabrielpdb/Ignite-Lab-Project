8562007d31f4014579f501da5733ea9f6b4c7305
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2022-06-24 22:05:15 -0300

    Lesson 4 done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.png)'
+      },
       colors: {
         fontFamily: {
           sans: 'Roboto, sans-serif'
